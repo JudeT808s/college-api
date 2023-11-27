08cbdd9eb23b69865acfb5f64db374c155cde638
Jude <n00213344@iadt.ie>
2023-11-27 23:52:58 +0000

    work on login and index/show

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+//import pages
 import CoursesIndex from '../src/pages/courses/Index'
+//import components
+import LoginForm from './components/LoginForm';
 function App() {
   return (
     <Router>
     <Routes>
-      <Route path="/" element={<CoursesIndex />}/>
+        <Route path="/" element={<CoursesIndex />} />
+        {/* //Change this to the default path later */}
+      <Route path="/login" element={<LoginForm />}/>
       </Routes>
       </Router>
   );
---
+++ src/components/LoginForm.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const LoginForm = (/*{authenticated, onAuthenticated}*/) => {
+
+    const errorStyle = {
+        color: 'red'
+    }
+
+    const [errorMessage, setErrorMessage] = useState("");
+    const [form, setForm] = useState({
+        name: "joe",
+        email: "test@test.com",
+        password: "Secret123"
+    });
+
+    const handleClick = () => {
+        console.log("clicked");
+
+        axios.post('https://college-api.vercel.app/register', {
+            name: form.name,
+            email: form.email,
+            password: form.password
+        })
+        .then(response =>{
+            console.log(response.data);
+            // onAuthenticated(true, response.data.token)
+        })
+        .catch(err => {
+            console.error(err)
+            // setErrorMessage(err.response.data.message)
+
+        })
+    };
+
+    const handleForm = (e) => {
+        setForm(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+
+        }));
+    };
+
+    return (
+        <>
+            Name: <input onChange={handleForm} type="text" name="name" value={form.name} /> <br />
+            Email: <input onChange={handleForm} type="text" name="email" value={form.email} /> <br />
+            Password: <input onChange={handleForm} type="password" name="password" value={form.password} />
+
+            <button onClick={handleClick}>Submit</button>
+            <p style={errorStyle}>{errorMessage}</p>
+        </>
+    );
+};
+
+export default LoginForm;
