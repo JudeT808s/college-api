248413e07a6a496cec7bb59beda89ee0c4f7def3
jude <n00213344'iadt.ie>
2023-12-04 17:05:51 +0000

    configured daisy.ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,34 @@
 import { Link, useNavigate } from 'react-router-dom'
 
-const Navbar = ({authenticated, onAuthenticated}) => {
+const Navbar = ({ authenticated, onAuthenticated }) => {
     const navigate = useNavigate();
-    const logout = () =>{
+    const logout = () => {
         onAuthenticated()
     }
     return (
-        
         <>
-            <Link to="/">Home</Link>
-            {(authenticated)? (<button onClick= {() => onAuthenticated(false)}>Logout</button>) : ""}
-            <Link to="/courses/create">Create</Link>
-            {(authenticated)? (<button onClick= {() => onAuthenticated(false)}>Create</button>) : ""}
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <Link to="/">Home</Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Link</a></li>
+                        <li>
+                            <details>
+                                <summary>
+                                    Parent
+                                </summary>
+                                <ul className="p-2 bg-base-100 rounded-t-none">
+                                    {(authenticated) ? (<button onClick={() => onAuthenticated(false)}>Logout</button>) : ""}
+                                    <Link to="/courses/create">Create</Link>
+                                    {(authenticated) ? (<button onClick={() => onAuthenticated(false)}>Create</button>) : ""}
+                                </ul>
+                            </details>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import './assets/index.css';
+import './index.css'
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
